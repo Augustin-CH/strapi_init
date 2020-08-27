6d2515d4f2da87346430d9b86aa2c7519db7e301
root <root@localhost.localdomain>
2020-08-27 17:04:05 +0200

    adding files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '40a2764f4c36f622fafc1e5ac481bf36'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'da5657a8-39e2-43db-81ce-b1ef53f3ef04'
+};
